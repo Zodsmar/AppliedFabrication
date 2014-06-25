93f57ec5b136f262001e032eb74d81e21615c986
Zodsmar <zodsmar@gmail.com>
2014-06-25 13:55:06 -0400

    Beginning of a Complete Re-write of AF

---
+++ src/main/java/com/zodsmar/appliedfabrication/AppliedFabrication.java
@@ -0,0 +1,8 @@
+package com.zodsmar.appliedfabrication;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid="AppliedFabrication", name="Applied Fabrication", version="1.7.2-1.0")
+public class AppliedFabrication {
+
+}
